d54ba372fa8652235379bc3f8f12c90c4a8f5709
Sean Chester <sean.chester@ntnu.no>
2018-12-31 13:44:02 +0100

    ✨ Brute force 2d solver handles trivial edge case

--- code/src/brute-force/regret-solver-2d.cpp
+++ code/src/brute-force/regret-solver-2d.cpp
@@ -1,11 +1,32 @@
 #include "k-regret/brute-force/regret-solver-2d.h"
 
+
 namespace kregret {
+namespace // anonymous
+{
+
+Solution pad_with_zeroes( Dataset< 2 > const& data, size_t const result_size )
+{
+  Solution s( result_size, 0.0f );
+  std::transform(
+    data.cbegin(),
+    data.cend(),
+    s.begin(),
+    []( auto const& p ){ return p.first; }
+  );
+
+  return s;
+}
+
+} // namespace anonymous
+
 namespace BruteForce {
 
 
 Solution Solver2d::representative_set( size_t const r, size_t const k ) const
 {
+  if( r >= data.size() ) { return pad_with_zeroes( data, r ); }
+
   return {};
 }
 
