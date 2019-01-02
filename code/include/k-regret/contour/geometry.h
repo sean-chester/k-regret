ef7459d7f10f2c84073f5beca3de8be488369d65
Sean Chester <sean.chester@ntnu.no>
2019-01-02 12:17:14 +0100

    ✨ Add utility method to compute intersection point of dual lines

--- code/include/k-regret/contour/geometry.h
+++ code/include/k-regret/contour/geometry.h
@@ -18,10 +18,10 @@ namespace geom{
  * std::nullopt if the lines are parallel (or coincident); i.e., have no unique
  * intersection point.
  */
+inline
 std::optional< Point< 2 > > dual_line_intersection( Point< 2 > const a, Point< 2 > const b )
 {
-  auto const y_diff = a[ 1 ] - b[ 1 ];
-  auto const shift = b[ 0 ] * b[ 1 ] - a[ 0 ] * a[ 1 ];
+  auto const shift = a[ 0 ] * b[ 1 ] - b[ 0 ] * a[ 1 ];
 
   if( shift == 0 ) /* parallel or coincident */ 
   {
@@ -29,14 +29,22 @@ std::optional< Point< 2 > > dual_line_intersection( Point< 2 > const a, Point< 2
   }
   else if( a[ 1 ] == 0 ) /* easy case: dual line of a is vertical */
   {
-    return Point< 2 >{ ( 1.0f - b[ 0 ] * a[ 0 ] ) / b [ 1 ], 0.0f };
+    assert( b[ 1 ] != 0.0f );
+    return Point< 2 >{ a[ 0 ], ( 1.0f - b[ 0 ] * a[ 0 ] ) / b [ 1 ] };
   }
+  else if( b[ 1 ] == 0 ) /* another easy case: dual line of b is vertical */
+  {
+    assert( a[ 1 ] != 0.0f );
+    return Point< 2 >{ b[ 0 ], ( 1.0f - a[ 0 ] * b[ 0 ] ) / a [ 1 ] };
+  }
+  else
+  {
+    auto const y_diff = b[ 1 ] - a[ 1 ];
+    auto const x = y_diff / shift;
+    auto const y = ( 1.0f - a[ 0 ] * x ) / a [ 1 ];
 
-  auto const x = y_diff / shift;
-  return Point< 2 >{
-    x,
-    ( 1.0f - a[ 0 ] * x ) / a [ 1 ]
-  };
+    return Point< 2 >{ x, y };
+  }
 }
 
 } // namespace geom
