7ef516b0dda089a54a58fd52b279a8941b67962a
Sean Chester <sean.chester@ntnu.no>
2019-01-04 11:01:36 +0100

    🚸 Add output overloads for data point structs

--- code/include/k-regret/common/data-point.h
+++ code/include/k-regret/common/data-point.h
@@ -6,9 +6,10 @@
 #ifndef KREGRET_COMMON_DATA_POINT
 #define KREGRET_COMMON_DATA_POINT
 
-#include <cstddef>
-#include <array>
-#include <vector>
+#include <cstddef>  // uint32_t
+#include <array>    // std::array
+#include <vector>   // std::vector
+#include <iostream> // std::ostream
 
 namespace kregret {
 
@@ -28,6 +29,20 @@ template < size_t D >
 
 template < size_t D >
   using Dataset = std::vector< DataPoint< D > >;
+
+inline
+std::ostream &
+  operator << ( std::ostream & stream, Point< 2 > const& p )
+  {
+    return stream << "«" << p[ 0 ] << " " << p[ 1 ] << "»";
+  }
+
+inline
+std::ostream &
+  operator << ( std::ostream & stream, DataPoint< 2 > const& p )
+  {
+    return stream << "(" << p.id << ", " << p.coords << ")";
+  }
 
 } // namespace kregret
 
